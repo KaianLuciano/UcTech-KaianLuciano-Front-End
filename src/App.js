e8ca9836e9bf5d439a27a8e6fbc41c525dad1884
KaianLuciano <kaianlucianoalvesdossantos@gmail.com>
2023-06-29 18:20:37 -0300

    Adicionado Listar Empresas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import Button from '@material-ui/core/Button';
 import { BiTrash, BiUserPlus } from "react-icons/bi";
+import Table from 'react-bootstrap/Table';
 
 import './styles.css';
 
@@ -10,37 +11,65 @@ import api from './services/api';
 function App() {
 
   /*
-  15.684.3070001-26
-  24410913000144
-  00.280.2730002-18
+  24410913000144 = NU
+  15.375.357/0001-21 = Coco Bambu
+  11.405.679/0001-15 = Potiguar Caldos
   */
 
   const [inputSalvar, setInputSalvar] = useState('');
   const [inputBuscar, setInputBuscar] = useState('');
   const [empresa, setEmpresa] = useState({});
   const [showInputSalvar, setShowInputSalvar] = useState(false);
+  const [showTable, setShowTable] = useState(false);
   const [showInputBuscar, setShowInputBuscar] = useState(false);
   const [showMain, setShowMain] = useState(false);
+  const [listaEmpresas, setListaEmpresas] = useState([]);
 
   function validarCampoPesquisa(campo) {
-    var regex = /^[0-9.\/-]+$/; // Expressão regular para verificar se contém apenas números, pontos, barras e traços
-  
-    // Remove os caracteres '.', '-', e '/' do campo de pesquisa
-    var numerosCampo = campo.replace(/[^\d.\/-]/g, '');
-  
-    if (numerosCampo !== campo) {
-      throw new Error("O campo de pesquisa contém letras ou caracteres inválidos!");
-    }
-
-    if (numerosCampo.length === 0) {
+    if (campo.length === 0) {
       throw new Error("O campo de pesquisa está vazio!");
     }
   
   }
 
+  function formatarCNPJ(cnpj) {
+    if (cnpj && cnpj.length === 14) {
+      return cnpj.substring(0, 2) + '.' +
+             cnpj.substring(2, 5) + '.' +
+             cnpj.substring(5, 8) + '/' +
+             cnpj.substring(8, 12) + '-' +
+             cnpj.substring(12);
+    }
+    return cnpj;
+  }
+  
+  async function handleListarEmpresas() {
+    try {
+      handleShowTable()
+      const response = await api.get();
+      console.log(response)
+      setListaEmpresas(response.data.body);
+    } catch (error) {
+      alert('Erro ao obter a lista de empresas');
+      console.log(error);
+    }
+  }
+
   function validarCNPJ(cnpj) {
-    // Remove todos os caracteres não numéricos do CNPJ
+
+    var regexLetras = /[a-zA-Z]/;
+
+    if (regexLetras.test(cnpj)) {
+      return; // Retorna do método se houver letras no CNPJ
+    }
+
+    var numerosCNPJ;
+    if (typeof cnpj === 'string') {
+      numerosCNPJ = cnpj.replace(/[^\d]/g, '');
+    } 
+
     var numerosCNPJ = cnpj.replace(/[^\d]/g, '');
+    console.log("teste")
 
     // Verifica se o CNPJ possui 14 dígitos
     if (numerosCNPJ.length !== 14) {
@@ -53,14 +82,27 @@ function App() {
       validarCampoPesquisa(inputBuscar);
       validarCNPJ(inputBuscar)
 
-      var numerosCNPJ = inputBuscar.replace(/[^\d]/g, '');
+      if (typeof inputBuscar === 'string') {
+        var numerosCNPJ = inputBuscar.replace(/[^\d]/g, '');
+      } 
 
-      const response = await api.get(`${numerosCNPJ}`);
-      setEmpresa(response.data);
+      let response;
+
+      if (/^\d{14}$/.test(numerosCNPJ)) {
+        console.log(inputBuscar)
+        response = await api.get(`${numerosCNPJ}`);
+      } else {
+        response = await api.get(`/nome/${inputBuscar}`);
+      }
 
       if(response.data.statusCodeValue == 404) {
+        handleCancel();
+        console.log(response)
         throw new Error(response.data.body);   
       } else {
+        response.data.body.cnpj = formatarCNPJ(response.data.body.cnpj)
+        console.log(response.data.body.cnpj)
+        setEmpresa(response.data);
         setShowMain(true);
         setInputBuscar('');
       }
@@ -105,12 +147,14 @@ function App() {
     setShowInputSalvar(true);
     setShowInputBuscar(false)
     setShowMain(false);
+    setShowTable(false)
   }
 
   function handleShowInputBuscar() {
     setShowInputBuscar(true);
     setShowInputSalvar(false);
     setShowMain(false);
+    setShowTable(false)
   }
 
   function handleShowMain() {
@@ -123,6 +167,13 @@ function App() {
     setShowInputSalvar(false);
     setShowInputBuscar(false);
     setShowMain(false);
+    setShowTable(false)
+  }
+
+  function handleShowTable() {
+    setShowTable(true);
+    setShowInputBuscar(false)
+    setShowInputSalvar(false)
   }
 
   return (
@@ -139,9 +190,16 @@ function App() {
         </Button>
         <Button
           variant="contained"
+          style={{ marginRight: '10px' }}
           onClick={handleShowInputBuscar}
         >
           Buscar Empresa
+        </Button>
+        <Button
+          variant="contained"
+          onClick={handleListarEmpresas}
+        >
+          Listar Empresas Cadastradas
         </Button>
       </div>
 
@@ -179,7 +237,7 @@ function App() {
             <BiTrash size={25} color='#FFF'/>
           </button>
         </div>
-      )}
+      )}   
 
       {showMain && Object.keys(empresa).length > 0 && (
         <main className="main">
@@ -198,6 +256,22 @@ function App() {
           <span>Opção do Mei: {empresa.body.opcaoMei}</span>
         </main>
       )}
+
+      {showTable && Object.keys(listaEmpresas).length > 0 && (
+      <div className="table">
+        <h2>Lista de Empresas Cadastradas</h2>
+        
+          <ul>
+            {listaEmpresas.map((empresa) => (
+              <li key={empresa.id}>{empresa.razaoSocial}</li>
+            ))}
+          </ul>
+        
+      </div>
+      )}
+
+      
+
     </div>
   );
 }
